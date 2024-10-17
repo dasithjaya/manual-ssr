255597cb9a5f02f2411de719fa9656d85ed21363
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2024-10-18 00:01:21 +0530

    completed

--- client.js
+++ client.js
@@ -1,8 +1,4 @@
-const { readFileSync } = require("fs");
-const { createServer } = require("http");
-const { parse } = require("url");
-const { renderToString } = require("react-dom/server");
-const React = require("react");
+ReactDOM.hydrateRoot(document.getElementById("root"), <Home/>)
 
 const pizzas = [
   {
@@ -63,25 +59,3 @@ function MenuItem({ pizza }) {
     </li>
   );
 }
-
-const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
-const clientJS = readFileSync(`${__dirname}/client.js`, "utf-8");
-
-const server = createServer((req, res) => {
-  const pathName = parse(req.url, true).pathname;
-  if (pathName === "/") {
-    const renderedReact = renderToString(<Home />);
-    const html = htmlTemplate.replace("%%%CONTENT%%%", renderedReact);
-    res.writeHead(200, { "Content-type": "text/html" });
-    res.end(html);
-  } else if (pathName === "/client.js") {
-    res.writeHead(200, { "Content-type": "application/javascript" });
-    res.end(clientJS);
-  } else {
-    res.end("URL not found");
-  }
-});
-
-server.listen(8000, () => {
-  console.log("Server is listening on port 8000");
-});
